41109edbd6be3823e31511d754d2e97ad73ea5dd
saravatss <149174589+saravatss@users.noreply.github.com>
2024-03-25 05:57:21 +0300

    mistaken changes

--- src/components/VHeader.vue
+++ src/components/VHeader.vue
@@ -1,34 +1,24 @@
 <template>
-    <header class="v-header">
-        <v-container>
-            <v-row align="center" justify="beetwen" no-gutters>
-                    <v-sidebar-menu/>
-
-                    <v-logo/>
-
-                    <router-link 
-                        v-if="isAuth"
-                        to="/profile" 
-                        class="v-menu__item"
-                    >
-                        <template v-if="user">
-                            {{ user.firstname }}
-                        </template>
-                    </router-link>
-
-                    <router-link 
-                        v-else
-                        to="/auth/sign-in" 
-                        class="v-menu__item"
-                    >
-                        Вход
-                    </router-link>
-            </v-row>
-        </v-container>
-    </header>
-</template>
-
-<script setup>
+    <div class="box">
+      <header class="header">
+        <div class="right-side">
+          <img class="img" alt="Search" src="https://c.animaapp.com/MsujbGHX/img/search@4x.png" />
+          <img class="img" alt="Person" src="https://c.animaapp.com/MsujbGHX/img/person@4x.png" />
+          <img class="img" alt="Cart" src="https://c.animaapp.com/MsujbGHX/img/cart@4x.png" />
+        </div>
+        <div class="left-side">
+          <img class="menu" alt="Menu" src="https://c.animaapp.com/MsujbGHX/img/menu@4x.png" />
+          <div class="view">
+            <img class="img" alt="Idea" src="https://c.animaapp.com/MsujbGHX/img/idea@4x.png" />
+            <div class="text-wrapper">Идеи</div>
+          </div>
+        </div>
+        <img class="rectangle" alt="Rectangle" src="https://c.animaapp.com/MsujbGHX/img/rectangle-9266@2x.png" />
+      </header>
+    </div>
+  </template>
+  
+  <script setup>
     import { ref } from 'vue';
     import { useRouter } from 'vue-router';
     import { useAuth } from '@/composables';
@@ -50,19 +40,85 @@
             }
         });
     }
-</script>
-
-<style>
-    .v-header {
-        position: fixed;
-        top: 0;
-        left: 0;
-        right: 0;
-        z-index: 100;
-
-        background-color: #F0F0F2;
-
-        padding: 10px 0;
-        border-bottom: 1px solid;
-    }
-</style>
+  </script>
+  
+  <style>
+  .box {
+    height: 122px;
+    position: relative;
+    width: 1440px;
+  }
+  
+  .box .header {
+    background-color: #eff0f2;
+    height: 122px;
+    left: 0;
+    position: fixed;
+    top: 0;
+    width: 1440px;
+  }
+  
+  .box .right-side {
+    align-items: center;
+    display: inline-flex;
+    gap: 32px;
+    justify-content: center;
+    left: 1160px;
+    position: absolute;
+    top: 45px;
+  }
+  
+  .box .img {
+    height: 30px;
+    object-fit: cover;
+    position: relative;
+    width: 30px;
+  }
+  
+  .box .left-side {
+    align-items: center;
+    display: inline-flex;
+    gap: 46px;
+    justify-content: center;
+    left: 128px;
+    position: absolute;
+    top: 45px;
+  }
+  
+  .box .menu {
+    height: 28px;
+    object-fit: cover;
+    position: relative;
+    width: 30px;
+  }
+  
+  .box .view {
+    align-items: center;
+    display: inline-flex;
+    flex: 0 0 auto;
+    gap: 12px;
+    justify-content: center;
+    position: relative;
+  }
+  
+  .box .text-wrapper {
+    color: #000000;
+    font-family: "Raleway", Helvetica;
+    font-size: 24px;
+    font-weight: 500;
+    letter-spacing: -0.53px;
+    line-height: 27.6px;
+    position: relative;
+    white-space: nowrap;
+    width: fit-content;
+  }
+  
+  .box .rectangle {
+    height: 82px;
+    left: 663px;
+    object-fit: cover;
+    position: absolute;
+    top: 19px;
+    width: 115px;
+  }
+  </style>
